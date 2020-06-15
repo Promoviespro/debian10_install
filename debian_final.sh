7634d68dd69d25ac8c50d48028efdbd4e236b5b9
Zaken <61211705+Promoviespro@users.noreply.github.com>
2020-06-15 23:16:04 +0530

    Update debian_final.sh

--- debian_final.sh
+++ debian_final.sh
@@ -35,7 +35,7 @@ firewall-cmd --reload
 #nano /etc/ssh/sshd_config
 service ssh restart
 #apt -y install certbot
-#openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
 
 apt -y install nginx nginx-module-geoip nginx-module-geoip-dbg
 systemctl start nginx.service
